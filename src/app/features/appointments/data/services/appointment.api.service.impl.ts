ab486ae46cd743f18d62f83e97129aa693ddd18c
Alfredo Palacios <joseaph.1998@gmail.com>
2025-12-10 13:32:41 -0600

    feat: service and repo implemented to get appointments

--- src/app/features/appointments/data/services/appointment.api.service.impl.ts
+++ src/app/features/appointments/data/services/appointment.api.service.impl.ts
@@ -3,17 +3,27 @@ import {Observable} from 'rxjs';
 import {AvailableAppointmentsResponseModel} from '../models/available.appoinments.response.model';
 import {HttpClient} from '@angular/common/http';
 import {inject, Injectable} from '@angular/core';
+import {AppointmentDataResponseModel} from '../../../users/profile/data/models/appointments.data.response.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppointmentApiServiceImpl implements AppointmentApiService{
+export class AppointmentApiServiceImpl implements AppointmentApiService {
+  private httpClient: HttpClient = inject(HttpClient)
 
-  private httpClient : HttpClient = inject(HttpClient)
+  getPaginatedAppointments(page: number, perPage: number): Observable<AppointmentDataResponseModel> {
+    return this.httpClient.get<AppointmentDataResponseModel>(`api/appointments/get?page=${page}&perPage=${perPage}`, {
+      params: {
+        page,
+        perPage
+      }
+    })
+  }
+
 
   getAvailableDates(doctor: string, date: string): Observable<AvailableAppointmentsResponseModel> {
     return this.httpClient.get<AvailableAppointmentsResponseModel>(`api/doctors/${doctor}/available-times`, {
-      params : {
+      params: {
         date
       }
     });
--- src/app/features/appointments/domain/repositories/appointment.repository.ts
+++ src/app/features/appointments/domain/repositories/appointment.repository.ts
@@ -1,6 +1,9 @@
 import {Observable} from 'rxjs';
 import {AvailableAppointmentsResponseModel} from '../../data/models/available.appoinments.response.model';
+import {AppointmentDataResponseModel} from '../../../users/profile/data/models/appointments.data.response.model';
 
 export interface AppointmentRepository {
   getAvailableDates(doctor: string, date: string): Observable<AvailableAppointmentsResponseModel>
+
+  paginateAppointments(page: number | null | undefined, perPage: number | null | undefined): Observable<AppointmentDataResponseModel>
 }
